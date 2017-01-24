e525b78e63b1ca9652096dcae1245887577091e5
majuss <biomajuss@gmail.com>
2017-01-24 14:43:38 +0100

    initial commit

---
+++ summary.js
@@ -0,0 +1,139 @@
+'use strict';
+
+const http = require('request-easy').http;
+const req  = new http({hostname:'api.globalbioticinteractions.org'});
+
+const input = encodeURIComponent('homo sapiens');
+const interaction = 'hasPathogen';
+const limit = 10000;
+
+
+
+req.get({path: `/interaction?type=json.v2&interactionType=${interaction}&limit=${limit}&offset=0&sourceTaxon=${input}&field=target_taxon_path&field=target_taxon_external_id`}, (status, headers, body)=>{
+    body = JSON.parse(body);
+
+
+
+    //console.log(body);
+
+    for (const interactionResult of body) {
+        interactionResult.path = interactionResult.target_taxon_path.split(' | ');
+        delete interactionResult.target_taxon_path;
+        insertIntoTree(tree, interactionResult);
+    }
+
+    console.log(JSON.stringify(tree, false, 6));
+
+})
+
+const tree = {
+    _results: []
+}
+
+function insertIntoTree(tree, interactionResult) {
+
+    if (interactionResult.path.length === 0) {
+        tree._results.push(interactionResult);
+        delete interactionResult.path;
+        return;
+    }
+
+    const rank = interactionResult.path.shift();
+
+    if (!tree[rank]) {
+        tree[rank] = {_results: []};
+    }
+
+    insertIntoTree(tree[rank], interactionResult);
+}
+
+
+/*
+##      interaction     source     target
+## 1        preysOn   predator       prey
+## 2   preyedUponBy       prey   predator
+## 3     parasiteOf   parasite       host
+## 4    hasParasite       host   parasite
+## 5     pollinates pollinator      plant
+## 6   pollinatedBy      plant pollinator
+## 7     pathogenOf   pathogen       host
+## 8    hasPathogen      plant pollinator
+## 9     symbiontOf     source     target
+## 10 interactsWith     source     target
+*/
+
+function append_field(field_name){
+/*
+	LATITUDE("latitude"),
+    LONGITUDE("longitude"),
+    ALTITUDE("altitude"),
+    FOOTPRINT_WKT("footprintWKT"),
+    LOCALITY("locality"),
+    COLLECTION_TIME_IN_UNIX_EPOCH("collection_time_in_unix_epoch"),
+    STUDY_TITLE("study_title"),
+    INTERACTION_TYPE("interaction_type"),
+
+    TARGET_TAXON_NAME("target_taxon_name"),
+    SOURCE_TAXON_NAME("source_taxon_name"),
+
+    TARGET_TAXON_COMMON_NAMES("target_taxon_common_names"),
+    SOURCE_TAXON_COMMON_NAMES("source_taxon_common_names"),
+
+    TARGET_TAXON_EXTERNAL_ID("target_taxon_external_id"),
+    SOURCE_TAXON_EXTERNAL_ID("source_taxon_external_id"),
+
+    TARGET_TAXON_PATH("target_taxon_path"),
+    SOURCE_TAXON_PATH("source_taxon_path"),
+
+    TARGET_TAXON_PATH_RANKS("target_taxon_path_ranks"),
+    SOURCE_TAXON_PATH_RANKS("source_taxon_path_ranks"),
+
+    TARGET_TAXON_PATH_IDS("target_taxon_path_ids"),
+    SOURCE_TAXON_PATH_IDS("source_taxon_path_ids"),
+
+    TARGET_SPECIMEN_TOTAL_FREQUENCY_OF_OCCURRENCE("target_specimen_frequency_of_occurrence"),
+    TARGET_SPECIMEN_TOTAL_FREQUENCY_OF_OCCURRENCE_PERCENT("target_specimen_frequency_of_occurrence_percent"),
+    TARGET_SPECIMEN_TOTAL_VOLUME_ML("target_specimen_total_volume_ml"),
+    TARGET_SPECIMEN_TOTAL_VOLUME_PERCENT("target_specimen_total_volume_ml_percent"),
+    TARGET_SPECIMEN_TOTAL_COUNT("target_specimen_total_count"),
+    TARGET_SPECIMEN_TOTAL_COUNT_PERCENT("target_specimen_total_count_percent"),
+
+    TARGET_SPECIMEN_ID("tmp_and_unique_target_specimen_id"),
+    SOURCE_SPECIMEN_ID("tmp_and_unique_source_specimen_id"),
+
+    TARGET_SPECIMEN_PHYSIOLOGICAL_STATE("target_specimen_physiological_state"),
+    SOURCE_SPECIMEN_PHYSIOLOGICAL_STATE( "source_specimen_physiological_state"),
+
+    TARGET_SPECIMEN_BODY_PART("target_specimen_body_part"),
+    SOURCE_SPECIMEN_BODY_PART("source_specimen_body_part"),
+
+    SOURCE_SPECIMEN_LIFE_STAGE("source_specimen_life_stage"),
+    TARGET_SPECIMEN_LIFE_STAGE("target_specimen_life_stage"),
+
+    SOURCE_SPECIMEN_BASIS_OF_RECORD("source_specimen_basis_of_record"),
+    TARGET_SPECIMEN_BASIS_OF_RECORD("target_specimen_basis_of_record"),
+
+    TAXON_NAME("taxon_name"),
+    TAXON_COMMON_NAMES("taxon_common_names"),
+    TAXON_EXTERNAL_ID("taxon_external_id"),
+
+    TAXON_EXTERNAL_URL("taxon_external_url"),
+    TAXON_PATH("taxon_path"),
+    TAXON_PATH_IDS("taxon_path_ids"),
+    TAXON_PATH_RANKS("taxon_path_ranks"),
+    STUDY_URL("study_url"),
+    STUDY_DOI("study_doi"),
+    STUDY_CITATION("study_citation"),
+    STUDY_SOURCE_CITATION("study_source_citation"),
+    STUDY_SOURCE_ID("study_source_id"),
+    NUMBER_OF_DISTINCT_TAXA("number_of_distinct_taxa", "only available for /reports/* queries"),
+    NUMBER_OF_DISTINCT_TAXA_NO_MATCH("number_of_distinct_taxa_no_match", "only available for /reports/* queries"),
+    NUMBER_OF_SOURCES("number_of_sources", "only available for /reports/* queries"),
+    NUMBER_OF_STUDIES("number_of_studies", "only available for /reports/* queries"),
+    NUMBER_OF_INTERACTIONS("number_of_interactions", "available for /interaction queries by source/target taxon name and/or interactionType only"),
+    STUDY_SOURCE_DOI("study_source_doi"),
+    STUDY_SOURCE_FORMAT("study_source_format"),
+    STUDY_SOURCE_ARCHIVE_URI("study_source_archive_uri");
+    */
+}
+
