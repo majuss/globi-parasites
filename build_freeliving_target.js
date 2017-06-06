f2f974212a359d72ad23a027d0f6e8f59f66c933
majuss <klumpen@gmx.de>
2017-06-06 13:04:02 +0200

    added importer freeliving target

--- build_freeliving_target.js
+++ build_freeliving_target.js
@@ -4,10 +4,16 @@ const db = require('arangojs')();
 const collection = db.collection('otl_parasites_nodes');
 
 db.query(`for doc in interaction_tsv
-          filter doc.interactionTypeName == 'parasiteOf' ||
+          filter doc.interactionTypeName == 'preyedUponBy' ||
+          doc.interactionTypeName == 'ectoParasitoid' ||
+          doc.interactionTypeName == 'parasiteOf' ||
           doc.interactionTypeName == 'ectoParasiteOf' ||
           doc.interactionTypeName == 'kleptoparasiteOf' ||
-          doc.interactionTypeName == 'ectoParasitoid'
+          doc.interactionTypeName == 'visitsFlowersOf' ||
+          doc.interactionTypeName == 'endoparasitoidOf'
+          doc.interactionTypeName == 'parasitoidOf' ||
+          doc.interactionTypeName == 'endoparasiteOf' ||
+          doc.interactionTypeName == 'visitsFlowersOf' ||
           return doc`, {}, { ttl: 1000 * 3600 }).then(testAvailable); //filter for interaction; ie isparasyte
 
 function testAvailable(cursor) {
@@ -15,9 +21,9 @@ function testAvailable(cursor) {
 
     cursor.next().then(doc => {
         try {
-            const ottId = doc.sourceTaxonIds.match(/OTT\:(\d+)/)[1];
+            const ottId = doc.targetTaxonIds.match(/OTT\:(\d+)/)[1];
             writeNewRankPath(ottId);
-        } catch(e) {} //here goes code to handle entries without OTTID
+        } catch (e) { } //here goes code to handle entries without OTTID
         testAvailable(cursor);
     });
 }
@@ -30,10 +36,9 @@ function writeNewRankPath(ott) {
                     _from:concat('otl_parasites_nodes/', SPLIT(doc._from, '/')[1] ),
                     _to:concat('otl_parasites_nodes/',   SPLIT(doc._to,   '/')[1] )
             }) in otl_parasites_edges OPTIONS { ignoreErrors: true }`);
-
     db.query(`for doc in (FOR v,e IN OUTBOUND SHORTEST_PATH 'nodes_otl/304358' TO 'nodes_otl/${ott}' GRAPH 'otl' return v)
     filter doc
     insert merge(doc, {_id:concat('otl_parasites_nodes/', doc._key),
-                        parasite: doc._key == '${ott}' ? 1 : 0 }) in otl_parasites_nodes OPTIONS { ignoreErrors: true }`); //if doc.key == searched OTTID update state to parasite
+                        freeliving: doc._key == '${ott}' ? 1 : 0 }) in otl_parasites_nodes OPTIONS { ignoreErrors: true }`); //if doc.key == searched OTTID update state to parasite
 }
 return;
