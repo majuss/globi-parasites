1fdfbbb7d82748fedfc24b1617fa4c5aef15dbc4
majuss <majuss@superusers-MacBook-Pro.local>
2017-06-13 20:56:51 +0200

    typo

--- build_parasites-collection.js
+++ build_parasites-collection.js
@@ -26,7 +26,7 @@ function testAvailable(cursor) {
 }
 
 function writeNewRankPath(ott) {
-    console.log('writing' + ott);
+    console.log('writing: ' + ott);
     db.query(`for doc in (FOR v,e IN OUTBOUND SHORTEST_PATH 'nodes_otl/304358' TO 'nodes_otl/${ott}' GRAPH 'otl' return e)
     filter doc
     insert merge(doc, {_id:concat('otl_parasites_edges/', doc._key),
@@ -39,7 +39,8 @@ function writeNewRankPath(ott) {
 
     UPSERT { _key: '${ott}' }
     INSERT merge(doc, {_id:concat('otl_parasites_nodes/', doc._key),
-                        parasite: doc._key == '${ott}' ? 1 : 0 })
+                        parasite: doc._key == '${ott}' ? 1 : 0 },
+                        globi: doc._key == '${ott}' ? 1 : 0 )
     UPDATE { parasite: doc._key == '${ott}' ? 1 : 0,
              globi: doc._key == '${ott}' ? 1 : 0 } in otl_parasites_nodes OPTIONS { ignoreErrors: true }`);
 }
